6c70ce1c04b73eff845c545e3792efa360ca2d3a
Andrei Vereha <avereha@gmail.com>
2014-10-18 18:36:04 +0300

    added logging

--- monkeys/main.py
+++ monkeys/main.py
@@ -1,5 +1,8 @@
 
 import argparse
+import logging
+import logging.config
+from ConfigParser import ConfigParser
 from monkeyhorde import MonkeyHorde
 
 if __name__ == '__main__':
@@ -7,6 +10,8 @@ if __name__ == '__main__':
     parser.add_argument('--config', default='../monkey.config',
                         dest='config_file', help='configuration file')
     args = parser.parse_args()
-
-    runner = MonkeyHorde(args.config_file)
+    config = ConfigParser()
+    config.read(args.config_file)
+    logging.config.fileConfig(args.config_file)
+    runner = MonkeyHorde(config)
     runner.unleash()
