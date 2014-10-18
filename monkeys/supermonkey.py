d5eae95e87ae6309d375a4ed93ebe7e25d7bc9d9
Radu Ciorba <raduciorba@gmail.com>
2014-10-18 19:14:21 +0300

    add basic google compute engine inventory

--- monkeys/supermonkey.py
+++ monkeys/supermonkey.py
@@ -1,9 +1,31 @@
+import re
+from collections import defaultdict
+from subprocess import check_output
 import ConfigParser
 
-from collections import defaultdict
+
+class GCEMixin(object):
+    """ A basic plugin to interact with Google Compute Engine"""
+    def __init__(self):
+        super(GCEMixin, self).__init__()
+        self._cge_machines = None
+        self._gce_enabled = False
+        if self.config.has_section("gce"):
+            self._gce_enabled = True #bool(self.config.get("gce", "enabled"))
+            self._gce_pattern = self.config.get("gce", "pattern")
+
+    def get_vm_groups(self):
+        super_groups = super(GCEMixin, self).get_vm_groups()
+        if self._gce_enabled:
+            output = check_output("gcloud compute instances list".split())
+            for lines in output[1:]:
+                name, zone, _, _, external_ip, status = line.split()
+                if status == "RUNNING" and re.match(self._gce_pattern):
+                    super_groups["gce"].append(external_ip)
+        return super_groups
 
 
-class Monkey(object):
+class Monkey(GCEmixin, object):
 # Superclass for all monkey types
 
     def __init__(self, config_file, scheduler):
