e11fb59796c16ba39f4f19baf92a24da9a4e91e5
Radu Ciorba <raduciorba@gmail.com>
2014-10-19 11:44:53 +0300

    add basic gce inventory discovery

--- monkeys/supermonkey.py
+++ monkeys/supermonkey.py
@@ -3,29 +3,28 @@ from collections import defaultdict
 from subprocess import check_output
 import ConfigParser
 
-
 class GCEMixin(object):
     """ A basic plugin to interact with Google Compute Engine"""
-    def __init__(self):
-        super(GCEMixin, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(GCEMixin, self).__init__(*args, **kwargs)
         self._cge_machines = None
         self._gce_enabled = False
         if self.config.has_section("gce"):
             self._gce_enabled = True #bool(self.config.get("gce", "enabled"))
             self._gce_pattern = self.config.get("gce", "pattern")
 
-    def get_vm_groups(self):
-        super_groups = super(GCEMixin, self).get_vm_groups()
+    def get_all_ips(self):
+        super_ips = super(GCEMixin, self).get_all_ips()
         if self._gce_enabled:
-            output = check_output("gcloud compute instances list".split())
-            for lines in output[1:]:
-                name, zone, _, _, external_ip, status = line.split()
-                if status == "RUNNING" and re.match(self._gce_pattern):
-                    super_groups["gce"].append(external_ip)
-        return super_groups
+            output = check_output("gcloud compute instances list".split()).splitlines()
+            for line in output[1:]:
+                name, zone, _, _, external_ip, status = [el for el in line.split() if el]
+                if status == "RUNNING" and re.match(self._gce_pattern, name) is not None:
+                    super_ips.append(external_ip)
+        return super_ips
 
 
-class Monkey(GCEmixin, object):
+class SuperMonkey(object):
 # Superclass for all monkey types
 
     def __init__(self, config, scheduler):
@@ -47,3 +46,7 @@ class Monkey(GCEmixin, object):
 
     def time_of_the_monkey(self):
         raise NotImplemented
+
+
+class Monkey(GCEMixin, SuperMonkey):
+    pass
